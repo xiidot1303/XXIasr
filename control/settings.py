1d28319f5fff2fa0d634f35df988d5989ba8b66d
xiidot1303 <xii1303@inbox.ru>
2022-05-12 16:04:52 +0500

    database on sql

--- control/settings.py
+++ control/settings.py
@@ -65,16 +65,31 @@ WSGI_APPLICATION = 'control.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config.DB_NAME,
-        'USER': config.DB_USER,
-        'PASSWORD': config.DB_PASSWORD,
-        'HOST': config.DB_HOST,
-        'PORT': config.DB_PORT,
+if config.ENVIRONMENT == 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config.DB_NAME,
+            'USER': config.DB_USER,
+            'PASSWORD': config.DB_PASSWORD,
+            'HOST': config.DB_HOST,
+            'PORT': config.DB_PORT,
+        }
     }
-}
+
+else:
+    DATABASES = {
+
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+
+
+
+        }
+    }
+
+
 
 
 # Password validation
