7332abc2017908d4d9acfdeeb44545ace82292e1
xiidot1303 <xii1303@inbox.ru>
2022-05-21 18:49:27 +0500

    static root

--- control/settings.py
+++ control/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = ''
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
