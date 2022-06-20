841af9adcc32ed91cb9af6022169f5fc145e8121
xiidot1303 <xii1303@inbox.ru>
2022-06-20 11:41:50 +0500

    test jobs

--- scheduled_job/updater.py
+++ scheduled_job/updater.py
@@ -3,7 +3,7 @@ from scheduled_job import job, mailing, birthday
 
 def start():
     scheduler = BackgroundScheduler(timezone='Asia/Tashkent')
-    scheduler.add_job(job.alert_clients, 'cron', hour=5, minute=13, second=1)
-    scheduler.add_job(birthday.send_congragulation, 'cron', hour=7, minute=21, second=21)
+    scheduler.add_job(job.alert_clients, 'cron', hour=11, minute=44, second=1)
+    scheduler.add_job(birthday.send_congragulation, 'cron', hour=11, minute=44, second=21)
     scheduler.add_job(mailing.send_message, 'interval', minutes=7)
     scheduler.start()
