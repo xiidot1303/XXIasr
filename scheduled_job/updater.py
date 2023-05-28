0319e0f7366b9cbbc6361b6ac05061d46da533c3
xiidot1303 <xii1303@inbox.ru>
2023-05-28 06:03:29 +0500

    change time of scheduled jobs

--- scheduled_job/updater.py
+++ scheduled_job/updater.py
@@ -3,7 +3,7 @@ from scheduled_job import job, mailing, birthday
 
 def start():
     scheduler = BackgroundScheduler(timezone='Asia/Tashkent')
-    scheduler.add_job(job.alert_clients, 'cron', hour=5, minute=13, second=1)
-    scheduler.add_job(birthday.send_congragulation, 'cron', hour=7, minute=21, second=21)
+    scheduler.add_job(job.alert_clients, 'cron', hour=8, minute=0, second=0)
+    scheduler.add_job(birthday.send_congragulation, 'cron', hour=9, minute=21, second=21)
     scheduler.add_job(mailing.send_message, 'interval', minutes=7)
     scheduler.start()
